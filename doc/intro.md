e74162c9195fd5745b1ee6aa4c2ce6b9d3962cfd
Richard Clamp <richardc@unixbeard.net>
2014-02-04 00:19:46 +0100

    new project from `lein new app our-george`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to our-george
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/our_george/core_test.clj
@@ -0,0 +1,7 @@
+(ns our-george.core-test
+  (:require [clojure.test :refer :all]
+            [our-george.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
