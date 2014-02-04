f0ddc4d123b5da5c43e3c6cff9ab40711ce21d80
Richard Clamp <richardc@unixbeard.net>
2014-02-04 12:34:11 +0100

    port to TK 0.3.2

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [puppetlabs/trapperkeeper "0.1.1"]]
+                 [puppetlabs/trapperkeeper "0.3.2"]
+                 [puppetlabs/trapperkeeper-webserver-jetty7 "0.3.2"]]
   :main puppetlabs.trapperkeeper.main)
--- src/our_george/core.clj
+++ src/our_george/core.clj
@@ -10,14 +10,12 @@
   (route/resources "/")
   (route/not-found "Not found"))
 
-(defn on-shutdown
-  []
-  (log/info "Bye bye from Our George."))
-
 (defservice our-george-service
-  {:depends [[:config-service get-config]
-             [:webserver-service add-ring-handler]]
-   :provides [shutdown]}
-  (let [config (get-config)]
-    (add-ring-handler app "/"))
-  {:shutdown on-shutdown})
+  [[:WebserverService add-ring-handler]]
+  (init [this context]
+        (log/info "Hello from Our George.")
+        (add-ring-handler app "/")
+        context)
+  (stop [this context]
+        (log/info "Bye bye from Our George.")
+        context))
