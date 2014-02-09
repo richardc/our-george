84acb3737e2dceecfe708a7a6e8e612f1bcf8323
Richard Clamp <richardc@unixbeard.net>
2014-02-09 16:59:21 +0000

    add `deal-a-card-to` and iterate over players
    This avoid the extra messing around with the turn-order when dealing the
    cards.

--- src/our_george/model.clj
+++ src/our_george/model.clj
@@ -14,21 +14,25 @@
            :deck [Card]
            :turn-order [PlayerName]})
 
-;; This tweaks turn-order.  As long as you call it once per player, as
-;; deal-a-round does then this isn't a problem.
-(s/defn deal-a-card :- Game
-  [game :- Game]
-  (let [player (first (:turn-order game))]
-    (-> game
-        (update-in [:players player :hand] conj (first (:deck game)))
-        (update-in [:deck] rest)
-        (update-in [:turn-order] #(concat (rest %) [(first %)])))))
+(s/defn deal-a-card-to :- Game
+  "Consumes a card from the deck, puts it into a players hand"
+  [game :- Game
+   player :- PlayerName]
+  (-> game
+      (update-in [:players player :hand] conj (first (:deck game)))
+      (update-in [:deck] rest)))
 
 (s/defn deal-a-round :- Game
+  "Deals a new card into all players hands"
   [game :- Game]
-  (nth (iterate deal-a-card game) (count (:turn-order game))))
+  (loop [players (:turn-order game)
+         game game]
+    (if (empty? players) game
+        (recur (rest players)
+               (deal-a-card-to game (first players))))))
 
 (s/defn make-game :- Game
+  "Creates a new game and does initial deal"
   [players :- [PlayerName]]
   (let [game {:players (zipmap players (repeat {:hand '(), :score 0}))
               :deck (shuffle (range 52))
