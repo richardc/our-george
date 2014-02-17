93d27e47371fd47ee075a57834c65d3347c095bc
Richard Clamp <richardc@unixbeard.net>
2014-02-17 20:04:20 +0000

    switch back to clojure.test
    midje was actually letting me code up failing testcases too easily.  drop the
    use of this and just use the (testing "") form to add documentation to
    clojure.test tests

--- test/our_george/model_test.clj
+++ test/our_george/model_test.clj
@@ -1,38 +1,31 @@
 (ns our-george.model-test
   (:require [clojure.test :refer :all]
-            [midje.sweet :refer :all]
             [our-george.model :refer :all]
-            [schema.test]))
+            [schema.test])
+  (:use [slingshot.test]))
 
 (use-fixtures :once schema.test/validate-schemas)
 
 (deftest make-game-test
+  (testing "make-game fails when given integers"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"does not match schema"
+                          (make-game [0 1 2 3]))))
   (testing "make-game"
-    (let [game (make-game ["fred" "barney" "ted"])]
-      (is map? game)
-      (is (= (:turn-order game) ["fred" "barney" "ted"]))
-      (is (= (count (get-in game [:players "ted" :hand])) 5)))))
+    (let [players ["fred" "barney" "ted"]
+          game (make-game players)]
+      (is (= players (:turn-order game)))
+      (testing "ted will have 5 cards"
+        (is (= 5 (count (get-in game [:players "ted" :hand])))))
+      (testing "ted will have a score of 0"
+        (is (= 0 (get-in game [:players "ted" :score]))))
+      (testing "fred will be the storyteller"
+        (is (= "fred" (get-in game [:storyteller])))))))
 
-(fact "make-game expects a vector of PlayerName"
-  (make-game [0 1 2 3]) => throws)
-
-(fact "make-game"
+(deftest tell-a-story-test
   (let [players ["fred" "barney" "ted"]
-        game (make-game players)]
-    (fact "it will be a map"
-      game => map?)
-    (fact "it will have a deck"
-      game => (contains {:deck list})
-      game => (contains {:deck (has every? number?)}))
-    (fact "it will have a turn-order of the original players"
-      game => (contains {:turn-order players}))
-    (fact "it will have a map player states"
-      game => (contains {:players map?}))
-    (fact "ted will be a player"
-      (let [ted (get-in game [:players "ted"])
-            hand (:hand ted)
-            score (:score ted)]
-        (fact "with 5 cards"
-          hand => (n-of number? 5))
-        (fact "with a score of 0"
-          score => 0)))))
+          game (make-game players)]
+    (testing "only the storyteller should be able to tell"
+      (is (thrown+? #(= :our-george.model/illegal-move (:type %))
+                    (tell-a-story game "barney" 5 "Legendary"))))
+    (testing "fred can tell a story"
+      (is (= "Yabba dabba") (:story (tell-a-story game "fred" 5 "Yabba Dabba"))))))
